fe4c64de3fc5f2c34f8a6e4512b8e2b8958c77e2
Maria-Bianca Cindroi <bianca.cindroi@gmail.com>
2017-02-22 00:55:10 +0100

    Create PizzaSlice.java

---
+++ src/pizza/PizzaSlice.java
@@ -0,0 +1,24 @@
+package pizza;
+/**
+ * @author Team QuoBono
+ * HashCode 2017: Pizza problem
+ * Object representing the pizza slice. 
+ **/
+public class PizzaSlice {
+
+	int beginingRow = -1;
+	int endingRow = -1;
+	int beginingColumn = -1;
+	int endingColumn = -1;
+	
+	
+	public PizzaSlice(int beginingRow, int endingRow, int beginingColumn, int endingColumn) {
+		super();
+		this.beginingRow = beginingRow;
+		this.endingRow = endingRow;
+		this.beginingColumn = beginingColumn;
+		this.endingColumn = endingColumn;
+	}
+
+	
+}
