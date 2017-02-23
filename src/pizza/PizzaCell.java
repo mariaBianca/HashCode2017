70b6a4a7bd765d7ee7cf1ec47ecf7c3ef98c1dc9
Maria-Bianca Cindroi <bianca.cindroi@gmail.com>
2017-02-23 12:41:33 +0100

    Update and rename PizzaSlice.java to PizzaCell.java

--- src/pizza/PizzaCell.java
+++ src/pizza/PizzaCell.java
@@ -4,7 +4,7 @@ package pizza;
  * HashCode 2017: Pizza problem
  * Object representing the pizza slice. 
  **/
-public class PizzaSlice {
+public class PizzaCell {
 
 	int beginingRow = -1;
 	int endingRow = -1;
@@ -12,7 +12,7 @@ public class PizzaSlice {
 	int endingColumn = -1;
 	
 	
-	public PizzaSlice(int beginingRow, int endingRow, int beginingColumn, int endingColumn) {
+	public PizzaCell(int beginingRow, int endingRow, int beginingColumn, int endingColumn, char topping) {
 		super();
 		this.beginingRow = beginingRow;
 		this.endingRow = endingRow;
